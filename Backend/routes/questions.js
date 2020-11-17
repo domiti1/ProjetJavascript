78115aedf7a0868ae2f250959af4c656baa13d34
Domitien VISSOTSKY <domitien.vissotsky@student.vinci.be>
2020-11-17 12:57:06 +0100

    installation backend

---
+++ Backend/routes/questions.js
@@ -0,0 +1,41 @@
+"use strict";
+let express = require("express");
+let router = express.Router();
+let Question = require("../models/Question.js");
+
+
+// Create a new question: POST /api/questions/
+router.post("/", function (req, res) {  
+  let newquestion = new question(req.body);
+  newquestion.save();
+  return res.json(newquestion);
+});
+
+// Read all the existing questions : GET /api/questions/
+router.get("/",  function (req, res) { 
+  return res.json(question.list);
+});
+
+// Read an identified question : GET /api/questions/:id
+router.get("/:id", function (req, res) {
+  const questionFound = question.get(req.params.id);
+  if (!questionFound) return res.status(404).end();
+
+  return res.json(questionFound);
+});
+
+// Delete a question : DELETE /api/questions/:id
+router.delete("/:id", function (req, res) {
+  const questionDeleted = question.delete(req.params.id);
+  if (!questionDeleted) return res.status(404).end();
+  return res.json(questionDeleted);
+});
+
+// Update a question : PUT /api/questions/:id
+router.put("/:id", function (req, res) {
+  const questionUpdated = question.update(req.params.id, req.body);
+  if (!questionUpdated) return res.status(404).end();
+  return res.json(questionUpdated);
+});
+
+module.exports = router;
