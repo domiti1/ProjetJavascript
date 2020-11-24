ab51e5596d2194b659dac6620994ab0d37225fe1
Obey SENHAJI <obey.senhaji@student.vinci.be>
2020-11-24 13:23:08 +0100

    change selector on links, quizz needs auth

--- Frontend/public/Components/Selector.js
+++ Frontend/public/Components/Selector.js
@@ -2,11 +2,14 @@ let selector =
 `<div class="container">
         <div class="row mt-3">
     
-        
+            
             <div class="col">
                 <div class="border border-5 border-secondary rounded" id="div_violet_smoother">
                     <div class="m-3">
-                        <h5>Veuillez selectionner vos thèmes </h5>
+                        <h5>Veuillez selectionner un thèmes </h5>
+                    </div>
+                    <div class="m-3">
+                        <h6>Le Quizz commencera dès qu'un thème sera selectionné</h6>
                     </div>
                 </div>
 
@@ -14,95 +17,91 @@ let selector =
 
                 <div class="mt-2">
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="anime" id="defaultCheck1">
-                        <div class="border border-5 border-secondary rounded" id="div_orange_smoother">
+                       
+                       
                             <div class="m-2">
-                                <h5>Anime/Manga</h5>
+                            <button type="button" class="btn btn-secondary btn-lg" id="div_orange_smoother" onclick="window.location.href='/quizz/theme=anime'">Anime/Manga</button>
                             </div>
                  
                 
-                        </div>
+                       
                     </div>
                 </div>
+
                 <div class="mt-2">
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="geographie" id="defaultCheck1">
-                        <div class="border border-5 border-secondary rounded" id="div_vert">
+                       
+                       
                             <div class="m-2">
-                                <h5>Géographie</h5>
+                            <button type="button" class="btn btn-secondary btn-lg" id="div_vert" onclick="window.location.href='/quizz/theme=geographie'">Géographie</button>
                             </div>
                  
                 
-                        </div>
+                       
                     </div>
                 </div>
+
                 <div class="mt-2">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="histoire" id="defaultCheck1">
-                    <div class="border border-5 border-secondary rounded" id="div_violet">
-                        <div class="m-2">
-                            <h5>Histoire</h5>
-                        </div>
-             
-            
+                    <div class="form-check">
+                       
+                       
+                            <div class="m-2">
+                            <button type="button" class="btn btn-secondary btn-lg" id="div_violet" onclick="window.location.href='/quizz/theme=histoire'">Histoire</button>
+                            </div>
+                 
+                
+                       
                     </div>
                 </div>
 
                 <div class="mt-2">
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="art" id="defaultCheck1">
-                    <div class="border border-5 border-secondary rounded" id="div_blue">
+                   
+                   
                         <div class="m-2">
-                            <h5>Art/Littérature</h5>
+                        <button type="button" class="btn btn-secondary btn-lg" id="div_blue" onclick="window.location.href='/quizz/theme=art'">Art/Littérature</button>
                         </div>
              
             
-                    </div>
+                   
                 </div>
 
-                <div class="mt-2">
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="technologie" id="defaultCheck1">
-                    <div class="border border-5 border-secondary rounded" id="div_grey">
-                        <div class="m-2">
-                            <h5>Technologie</h5>
-                        </div>
-             
-            
-                    </div>
+                   
+                   
+                   <div class="m-2">
+                   <button type="button" class="btn btn-secondary btn-lg" id="div_grey" onclick="window.location.href='/quizz/theme=techno'">Technologie</button>
+                   </div>
+        
+       
+              
                 </div>
-
-                <div class="mt-2">
+            </div>
+            <div class="form-check">
+                   
+                   
+                   <div class="m-2">
+                   <button type="button" class="btn btn-secondary btn-lg" id="div_red" onclick="window.location.href='/quizz/theme=science'">Sciences</button>
+                   </div>
+        
+       
+              
+                </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="science" id="defaultCheck1">
-                    <div class="border border-5 border-secondary rounded" id="div_red">
-                        <div class="m-2">
-                            <h5>Sciences</h5>
-                        </div>
-             
-            
-                    </div>
-                </div>
-
-                <div class="mt-2">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="musique" id="defaultCheck1">
-                    <div class="border border-5 border-secondary rounded" id="div_brown">
-                        <div class="m-2">
-                            <h5>Musique</h5>
-                        </div>
-             
-            
-                    </div>
-                </div>
-
-                <div class="m-5 text-center"><button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='/login'">Tester ma culture G</button></div>
+                   
+                   
+                   <div class="m-2">
+                   <button type="button" class="btn btn-secondary btn-lg" id="div_brown" onclick="window.location.href='/quizz/theme=musique'">Musique</button>
+                   </div>
+        
+       
+              
                 </div>
 
 
             </div>
                 
-                
+            
                 
                     
                 
@@ -110,13 +109,19 @@ let selector =
             </div>
 
         </div>
-    </div>`;
+   
+    
+   `;
+
+
+
 let page = document.querySelector("#page");
 
 const Selector = () => {
   document.querySelector("#page_home").classList.remove('full-size');
   let page = document.querySelector("#page");
   return (page.innerHTML = selector);
+ 
 };
 
 export default Selector;
